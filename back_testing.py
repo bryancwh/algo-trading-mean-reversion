55c5ff430dcd570e53c5654c90d915569fe07dd1
Bryan Chen <bryancwh98@gmail.com>
2021-05-31 21:35:13 +0800

    First Commit

---
+++ back_testing.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+
+
+def backtest_dataframe(df):
+    position = 0
+    net_profit = 0
+    percentage_change = []
+    df['buy_date'] = ''
+    df['sell_date'] = ''
+
+    for i in df.index:
+        close = df["close"][i]
+        date = df['date'][i]
+
+        # Buy action
+        if df["signal"][i] == 1:
+            if(position == 0):
+                buy_price = close
+                position = 1
+                df.at[i, 'buy_date'] = date
+#                print(f"Buying at {str(buy_price)} on {str(date)}")
+
+        # Sell action
+        elif (df["signal"][i] == -1):
+            if(position == 1):
+                sell_price = close
+                bought = 0
+                position = 0
+                df.at[i, 'sell_date'] = date
+#                print(f"Selling at {str(sell_price)} on {str(date)}")
+
+                # Get percentage change of trade
+                pc = (sell_price/buy_price-1)*100
+                percentage_change.append(pc)
+                net_profit += (sell_price - buy_price)
+
+    # Calculate trade statistics
+    gains = 0
+    ng = 0
+    losses = 0
+    nl = 0
+    totalR = 1
+
+    for i in percentage_change:
+        if(i > 0):
+            gains += i
+            ng += 1
+        else:
+            losses += i
+            nl += 1
+        totalR = totalR * ((i/100)+1)
+
+    totalR = round((totalR-1)*100, 2)
+
+    if(ng > 0):
+        avgGain = round(gains/ng, 2)
+        maxR = round(max(percentage_change), 2)
+    else:
+        avgGain = 0
+        maxR = "undefined"
+
+    if(nl > 0):
+        avgLoss = round(losses/nl, 2)
+        maxL = round(min(percentage_change), 2)
+    else:
+        avgLoss = 0
+        maxL = "undefined"
+
+    if(ng > 0 or nl > 0):
+        win_rate = round((ng/(ng+nl))*100, 2)
+    else:
+        win_rate = 0
+
+    print('Evaluation Metrics:')
+    print('-----------------------------------')
+    print(f"Number of Trades: {ng+nl}")
+    print(f"Number of Gains: {ng}")
+    print(f"Number of Losses: {nl}")
+    print(f"Total Returns: {totalR}%")
+    print(f"Win Rate: {win_rate}%")
+    print(f"Average Gain: {avgGain}%")
+    print(f"Average Loss: {avgLoss}%")
+    print(f"Max Return: {maxR}%")
+    print(f"Max Loss: {maxL}%")
+    print()
